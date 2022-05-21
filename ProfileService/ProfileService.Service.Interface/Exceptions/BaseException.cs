890d736b98082af2a89e617ff2fc22284dbd03d7
Ksenija Prcic <ksenija.prcic1998@gmail.com>
2022-05-21 16:50:41 +0200

    feat: Add Exceptions folder and BaseException

---
+++ ProfileService/ProfileService.Service.Interface/Exceptions/BaseException.cs
@@ -0,0 +1,10 @@
+﻿namespace ProfileService.Service.Interface.Exceptions
+{
+	public class BaseException : Exception
+	{
+		public int StatusCode { get; set; }
+
+		public BaseException(string message) : base(message) { }
+	}
+}
+
