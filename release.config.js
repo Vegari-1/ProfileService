919bebcd9b9b548528470dd4e9558dfcffb8c365
Eva Janković <evaj.evadesign@gmail.com>
2022-05-24 11:49:59 +0200

    feat: Create semantic-release config

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	branches: ["develop", "main"],
+	repositoryUrl: "https://github.com/Vegari-1/ProfileService",
+	plugins: [
+		"@semantic-release/commit-analyzer", // analizira komit poruke i odredjuje narednu verziju
+		"@semantic-release/release-notes-generator", // generise release notes na osnovu komita
+		"@semantic-release/github" // kreira github release
+	]
+}
