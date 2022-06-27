22c05f0d18bdbcfd6bd0adf1a7a6e29ae600585d
Eva Janković <evaj.evadesign@gmail.com>
2022-06-27 16:10:11 +0200

    feat: Publish Docker image to DockerHub

--- release.config.js
+++ release.config.js
@@ -2,8 +2,9 @@ module.exports = {
 	branches: ["main", {"name": "develop", "prerelease": true}],
 	repositoryUrl: "https://github.com/Vegari-1/ProfileService",
 	plugins: [
-		"@semantic-release/commit-analyzer", // analizira komit poruke i odredjuje narednu verziju
-		"@semantic-release/release-notes-generator", // generise release notes na osnovu komita
-		"@semantic-release/github" // kreira github release
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/github",
+		"semantic-release-export-data"
 	]
 }
