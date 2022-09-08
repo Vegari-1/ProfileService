6d6b6cb67a7d15037efeaa80a814c361ac7514ad
Eva Janković <evaj.evadesign@gmail.com>
2022-09-08 17:16:52 +0200

    test: add integration testing with testcontainers

---
+++ ProfileService/ProfileService.IntegrationTests/IntegrationWebApplicationFactory.cs
@@ -0,0 +1,49 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Configurations;
+using DotNet.Testcontainers.Containers;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProfileService.IntegrationTests
+{
+    public class IntegrationWebApplicationFactory<TProgram, TDbContext> : WebApplicationFactory<TProgram>, IAsyncLifetime
+    where TProgram : class where TDbContext : DbContext
+    {
+        public readonly TestcontainerDatabase container;
+
+        public IntegrationWebApplicationFactory()
+        {
+            container = new TestcontainersBuilder<PostgreSqlTestcontainer>()
+                .WithDatabase(new PostgreSqlTestcontainerConfiguration
+                {
+                    Database = "test_db",
+                    Username = "postgres",
+                    Password = "postgres",
+                })
+                .WithImage("postgres:11")
+                .WithName("postgres")
+                .WithCleanUp(true)
+                .Build();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                services.RemoveDbContext<TDbContext>();
+                services.AddDbContext<TDbContext>(options => { options.UseNpgsql(container.ConnectionString); });
+                services.EnsureDbCreated<TDbContext>();
+            });
+        }
+
+        public async Task InitializeAsync() => await container.StartAsync();
+
+        public new async Task DisposeAsync() => await container.DisposeAsync();
+
+    }
+}
---
+++ ProfileService/ProfileService.IntegrationTests/ProfileControllerTests.cs
@@ -0,0 +1,166 @@
+using Newtonsoft.Json;
+using ProfileService.Dto;
+using ProfileService.Model;
+using ProfileService.Repository;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProfileService.IntegrationTests
+{
+    public class ProfileControllerTests : IClassFixture<IntegrationWebApplicationFactory<Program, AppDbContext>>
+    {
+        private readonly IntegrationWebApplicationFactory<Program, AppDbContext> _factory;
+        private readonly HttpClient _client;
+
+        public ProfileControllerTests(IntegrationWebApplicationFactory<Program, AppDbContext> factory)
+        {
+            _factory = factory;
+            _client = _factory.CreateClient();
+        }
+
+        private static readonly string tableName = "Profiles";
+        private static readonly Guid id = Guid.NewGuid();
+        private static readonly Guid userId = Guid.NewGuid();
+        private static readonly bool profilePublic = true;
+        private static readonly string name = "John";
+        private static readonly string surname = "Smith";
+        private static readonly string username = "johnsmith";
+        private static readonly string email = "email@example.com";
+        private static readonly string phone = "1234567890";
+        private static readonly string gender = "Female";
+        private static readonly DateTime dateOfBirth = DateTime.Now;
+        private static readonly string biography = "bio";
+        private static readonly string picture = "base64";
+        
+        private static readonly string query = "Joh";
+
+        [Fact]
+        public async Task Create_CorrectData_ProfileResponse()
+        {
+            // Given
+            ProfileRequest profileRequest = new ProfileRequest()
+            {
+                UserId = userId,
+                Public = profilePublic,
+                Name = name,
+                Surname = surname,
+                Username = username,
+                Email = email,
+                Phone = phone,
+                Gender = gender,
+                DateOfBirth = dateOfBirth,
+                Biography = biography,
+                //Picture = picture
+            };
+            var requestContent = new StringContent(JsonConvert.SerializeObject(profileRequest), Encoding.UTF8, "application/json");
+
+            // When
+            var response = await _client.PostAsync("/api/profile", requestContent);
+
+            // Then
+            response.EnsureSuccessStatusCode();
+            var responseContentString = await response.Content.ReadAsStringAsync();
+            var responseContentObject = JsonConvert.DeserializeObject<ProfileResponse>(responseContentString);
+            Assert.NotNull(responseContentObject);
+            Assert.Equal(profilePublic, responseContentObject.Public);
+            Assert.Equal(name, responseContentObject.Name);
+            Assert.Equal(surname, responseContentObject.Surname);
+            Assert.Equal(username, responseContentObject.Username);
+            Assert.Equal(email, responseContentObject.Email);
+            Assert.Equal(phone, responseContentObject.Phone);
+            Assert.Equal(gender, responseContentObject.Gender);
+            Assert.Equal(dateOfBirth, responseContentObject.DateOfBirth);
+            Assert.Equal(biography, responseContentObject.Biography);
+            Assert.Equal(1L, _factory.CountTableRows(tableName));
+
+            // Rollback
+            _factory.DeleteById(tableName, responseContentObject.Id);
+        }
+
+        [Fact]
+        public async void GetByPublic_isPublic_IEnumerableProfileResponseSimple()
+        {
+            // Given
+            Profile profile = new Profile()
+            {
+                Id = id,
+                UserId = userId,
+                Public = profilePublic,
+                Name = name,
+                Surname = surname,
+                Username = username,
+                Email = email,
+                Phone = phone,
+                Gender = gender,
+                DateOfBirth = dateOfBirth,
+                Biography = biography
+            };
+            _factory.Insert(tableName, profile);
+
+            // When
+            var response = await _client.GetAsync("/api/profile?isPublic=true");
+
+            // Then
+            response.EnsureSuccessStatusCode();
+            var responseContentString = await response.Content.ReadAsStringAsync();
+            var responseContentObject = JsonConvert.DeserializeObject<List<ProfileSimpleResponse>>(responseContentString);
+            Assert.NotNull(responseContentObject);
+            Assert.Single(responseContentObject);
+            var responseObject = responseContentObject[0];
+            Assert.Equal(id, responseObject.Id);
+            Assert.Equal(name, responseObject.Name);
+            Assert.Equal(surname, responseObject.Surname);
+            Assert.Equal(username, responseObject.Username);
+            Assert.Null(responseObject.Picture);
+
+            // Rollback
+            _factory.DeleteById(tableName, id);
+        }
+
+        [Fact]
+        public async void SearchProfiles_IsPublicAndNameContains_IEnumerableProfileResponseSimple()
+        {
+            // Given
+            Profile profile = new Profile()
+            {
+                Id = id,
+                UserId = userId,
+                Public = profilePublic,
+                Name = name,
+                Surname = surname,
+                Username = username,
+                Email = email,
+                Phone = phone,
+                Gender = gender,
+                DateOfBirth = dateOfBirth,
+                Biography = biography
+            };
+            _factory.Insert(tableName, profile);
+
+            // When
+
+            var response = await _client.GetAsync("/api/profile?isPublic=true&query=" + query);
+
+            // Then
+            response.EnsureSuccessStatusCode();
+            var responseContentString = await response.Content.ReadAsStringAsync();
+            var responseContentObject = JsonConvert.DeserializeObject<List<ProfileSimpleResponse>>(responseContentString);
+            Assert.NotNull(responseContentObject);
+            Assert.Single(responseContentObject);
+            var responseObject = responseContentObject[0];
+            Assert.Equal(id, responseObject.Id);
+            Assert.Equal(name, responseObject.Name);
+            Assert.Equal(surname, responseObject.Surname);
+            Assert.Equal(username, responseObject.Username);
+            Assert.Null(responseObject.Picture);
+            Assert.Contains(query, responseObject.Name);
+
+            // Rollback
+            _factory.DeleteById(tableName, id);
+        }
+    }
+}
