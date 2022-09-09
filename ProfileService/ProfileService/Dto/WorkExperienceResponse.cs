1c94facf894cbf686d1a1dd5c45355ab92a291f4
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-09-09 19:52:44 +0200

    fix: change response object end date

--- ProfileService/ProfileService/Dto/WorkExperienceResponse.cs
+++ ProfileService/ProfileService/Dto/WorkExperienceResponse.cs
@@ -6,6 +6,6 @@
         public string Position { get; set; }
         public string Company { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
     }
 }
