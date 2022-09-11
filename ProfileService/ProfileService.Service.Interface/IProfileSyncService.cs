69714cf0c910027bd0d85101f2aa1d1e391701c5
Ksenija Prćić <ksenija.prcic1998@gmail.com>
2022-09-11 02:19:25 +0200

    feat: publish profile changes via bus service

---
+++ ProfileService/ProfileService.Service.Interface/IProfileSyncService.cs
@@ -0,0 +1,10 @@
+﻿using BusService;
+using BusService.Contracts;
+using ProfileService.Model;
+
+namespace ProfileService.Service.Interface
+{
+    public interface IProfileSyncService : ISyncService<Profile, ProfileContract>
+    {
+    }
+}
---
+++ ProfileService/ProfileService.Service/ProfileSyncService.cs
@@ -0,0 +1,36 @@
+﻿using BusService;
+using BusService.Contracts;
+using BusService.Routing;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ProfileService.Model;
+using ProfileService.Service.Interface;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileService.Service
+{
+    public class ProfileSyncService : ConsumerBase<Profile, ProfileContract>, IProfileSyncService
+    {
+        private readonly IMessageBusService _messageBusService;
+
+        public ProfileSyncService(IMessageBusService messageBusService, ILogger<ProfileSyncService> logger) : base(logger)
+        {
+            _messageBusService = messageBusService;
+        }
+
+        public override Task PublishAsync(Profile entity, string action)
+        {
+            var serialized = JsonConvert.SerializeObject(entity);
+            var bData = Encoding.UTF8.GetBytes(serialized);
+            _messageBusService.PublishEvent(SubjectBuilder.Build(Topics.Profile, Events.Updated), bData);
+            return Task.CompletedTask;
+        }
+
+        public override Task SynchronizeAsync(ProfileContract entity, string action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
